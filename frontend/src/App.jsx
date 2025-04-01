1c13b8c00b9b9d846ae36b123886d1e475aed255
Поцелуйко Иван Анатольевич <119080618+PeculiarRP@users.noreply.github.com>
2025-04-01 18:07:04 +0300

    Update Metric Log

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,21 @@ import React, {useEffect } from 'react'
 import './App.css'
 import './utils/TableCreator.js'
 import './utils/API.js'
-import { metrics } from "./utils/prome-proxy.js";
 import tableCreator from "./utils/TableCreator.js";
 import api from "./utils/API.js";
+import { register, Gauge, Counter} from "prom-client";
 
+const pageLoadDuration = new Gauge({
+    name: "page_load_duration_seconds",
+    help: "Page load duration in seconds",
+    registers: [register],
+})
+
+const pageViews = new Counter({
+    name: 'page_views_total',
+    help: 'Total number of page views',
+    registers: [register],
+});
 
 function App() {
     // const  tmpList = [
@@ -13,6 +24,15 @@ function App() {
     //     {ID: "2", Name: "Petr", Surname: "Petrov", Job: "ALGO"},
     // ];
     // const [data, setData] = useState([]);
+
+    function trackPageLoadTime(){
+        const start = performance.now();
+        window.onload = () => {
+            const end = performance.now();
+            const duration = (end - start) / 1000;
+            pageLoadDuration.set(duration);
+        }
+    }
 
     function updateTable(data) {
         let oldTable = document.querySelector("table");
@@ -28,10 +48,19 @@ function App() {
         updateTable(req.data);
     }
 
+    const sendMetrics = () => {
+        setInterval(() => {
+            api.post('/metrics', {
+                page_load_duration: pageLoadDuration.get().values[0]?.value || 0,
+                page_views_total: pageViews.get().values[0]?.value || 0,
+            }).catch(error => console.error('Error sending metrics:', error));
+        }, 5000);
+    };
+
     useEffect(() => {
+        trackPageLoadTime();
         getData();
-        metrics.pageViews.inc();
-        // updateTable(data);
+        sendMetrics();
     }, []);
 
   return (
