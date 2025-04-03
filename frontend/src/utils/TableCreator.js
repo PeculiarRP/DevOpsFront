2339b0f427d8c92f26d45487c6f428aa40e2bb07
Поцелуйко Иван Анатольевич <119080618+PeculiarRP@users.noreply.github.com>
2025-04-03 15:13:45 +0300

    Fix

--- frontend/src/utils/TableCreator.js
+++ frontend/src/utils/TableCreator.js
@@ -7,12 +7,12 @@ function tableCreator(tableData) {
     let thead = document.createElement("thead");
     let headerRow = document.createElement("tr");
 
-    let headers = ["ID", "Имя", "Фамилия", "Должность", "Добавить"]
-    let listHeader = ["ID", "Name", "Surname", "Job", "Добавить"]
+    let headers = ["ID", "Имя", "Фамилия", "Должность", "Добавить запись"]
+    let listHeader = ["ID", "Name", "Surname", "Job", "Добавить запись"]
 
     headers.forEach(header => {
         let th = document.createElement("th");
-        if (header === "Добавить") {
+        if (header === "Добавить запись") {
             let btn = document.createElement("button");
             btn.textContent = header;
             btn.addEventListener("click", () => {
