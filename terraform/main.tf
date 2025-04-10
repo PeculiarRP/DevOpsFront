8a940dfd36913f630a27565c2b744824a8262e2c
Денис Ермолаев <74070947+den251965@users.noreply.github.com>
2025-04-10 17:00:15 +0300

    100425

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,16 +3,18 @@ resource "yandex_compute_disk" "boot-disk-1" {
   type     = "network-hdd"
   zone     = "ru-central1-d"
   size     = "20"
-  image_id = "fd8sq3r72r0caul0fn37"
+  # image_id = "fd8sq3r72r0caul0fn37"
+  image_id = "fd89esja5ecg35fu6n2t" #container-optimized-image
 }
 
 resource "yandex_compute_instance" "vm-1" {
-  name = "terraform1"
+  name        = "terraform1"
   platform_id = "standard-v2"
 
   resources {
-    cores  = 4
-    memory = 4
+    core_fraction = 50
+    cores  = 2
+    memory = 8
   }
 
   boot_disk {
@@ -25,8 +27,12 @@ resource "yandex_compute_instance" "vm-1" {
   }
 
   metadata = {
-    user-data = "${file("D:/ITMO/Devops/meta.txt")}"
+    user-data = "${file("D:/ITMO/Devops/meta.yaml")}"
   }
+
+  scheduling_policy {
+    preemptible = true
+  }  
 }
 
 resource "yandex_vpc_network" "network-1" {
@@ -38,6 +44,38 @@ resource "yandex_vpc_subnet" "subnet-1" {
   zone           = "ru-central1-d"
   network_id     = yandex_vpc_network.network-1.id
   v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+resource "null_resource" "transfer_docker_compose" {
+  connection {
+    type = "ssh"
+    user = "devopser"
+    host = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+    agent       = true
+    timeout     = "1m"
+  }  
+  provisioner "file" {
+    source      = "../docker-compose.yml"
+    destination = "/home/devopser/docker-compose.yml"
+  }
+  depends_on = [yandex_compute_instance.vm-1]
+}
+
+resource "null_resource" "docker_compose_run" {
+  connection {
+    type = "ssh"
+    user = "devopser"
+    host = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+    agent       = true
+    timeout     = "1m"
+  }    
+  provisioner "remote-exec" {
+    inline = [
+      "cd /home/devopser",
+      "sudo docker-compose up -d"
+    ]    
+  }
+  depends_on = [null_resource.transfer_docker_compose]
 }
 
 output "internal_ip_address_vm_1" {
