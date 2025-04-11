d3e71d43b802a2e234c1971156b148dfb253169f
Денис Ермолаев <74070947+den251965@users.noreply.github.com>
2025-04-11 14:16:54 +0300

    erh

--- frontend/src/utils/API.js
+++ frontend/src/utils/API.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-   // baseURL: "http://backand:8585/",
-     baseURL: "http://api/",
+    baseURL: "http://backand:8585/",
 })
 
 export default api;
