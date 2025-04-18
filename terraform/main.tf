48bed97dd52c720097a0f38c2e5c4af6076d2235
Денис Ермолаев <74070947+den251965@users.noreply.github.com>
2025-04-18 12:12:38 +0300

    180425

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ resource "yandex_compute_disk" "boot-disk-1" {
   name     = "boot-disk-1"
   type     = "network-hdd"
   zone     = "ru-central1-d"
-  size     = "20"
+  size     = "50"
   # image_id = "fd8sq3r72r0caul0fn37"
   image_id = "fd89esja5ecg35fu6n2t" #container-optimized-image
 }
@@ -12,9 +12,9 @@ resource "yandex_compute_instance" "vm-1" {
   platform_id = "standard-v2"
 
   resources {
-    core_fraction = 50
-    cores  = 2
-    memory = 8
+    # core_fraction = 50
+    cores  = 4
+    memory = 16
   }
 
   boot_disk {
@@ -61,6 +61,35 @@ resource "null_resource" "transfer_yml" {
   depends_on = [yandex_compute_instance.vm-1]
 }
 
+resource "null_resource" "miniqube_install" {
+  connection {
+    type = "ssh"
+    user = "devopser"
+    host = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+    agent       = true
+    timeout     = "1m"
+  }    
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt install curl apt-transport-https",
+      "sudo apt install ca-certificates",
+      "sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg",
+      # "sudo curl -LO \"https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl\"",
+      # "sudo apt install -y kubectl",
+      "sudo curl -LO https://dl.k8s.io/release/`curl -LS https://dl.k8s.io/release/stable.txt`/bin/linux/amd64/kubectl",
+      "sudo chmod +x ./kubectl",
+      "sudo mv ./kubectl /usr/local/bin/kubectl",
+      "sudo kubectl version",
+      "sudo kubectl create sa default",
+      "curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64",
+      "sudo install minikube-linux-amd64 /usr/local/bin/minikube",
+      "minikube version",
+    ]    
+  }
+  depends_on = [null_resource.transfer_yml]
+}
+
 resource "null_resource" "miniqube_run" {
   connection {
     type = "ssh"
@@ -71,26 +100,63 @@ resource "null_resource" "miniqube_run" {
   }    
   provisioner "remote-exec" {
     inline = [
-      # "cd /home/devopser/miniqube",
-      "sudo apt update",
-      "sudo apt install curl apt-transport-https",
-      "curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64",
-      "sudo install minikube-linux-amd64 /usr/local/bin/minikube",
-      "minikube version",
-      "sudo apt update && sudo apt install ca-certificates",
-      "sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg",
-      "curl -LO \"https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl\"",
-      # "sudo apt update && sudo apt install -y kubectl",
-      "chmod +x ./kubectl",
-      "sudo mv ./kubectl /usr/local/bin/kubectl",
-      "kubectl version --client --short",
-      "minikube status",
+      # "sudo usermod -aG docker $USER && newgrp docker",
+      "minikube config set cpus 4",
+      "minikube config set memory 8000",
       "minikube start --driver=docker",
-      "eval $(minikube docker-env)",
-      "kubectl apply -f /home/devopser/miniqube"
+      # "sudo minikube config set driver docker",
+      "minikube docker-env",
+      # "kubectl apply -f /home/devopser/miniqube",
     ]    
   }
-  depends_on = [null_resource.transfer_yml]
+  depends_on = [null_resource.miniqube_install]
+}
+
+resource "null_resource" "services_started" {
+  connection {
+    type = "ssh"
+    user = "devopser"
+    host = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+    agent       = true
+    timeout     = "1m"
+  }    
+  provisioner "remote-exec" {
+    inline = [
+      "cd /home/devopser/miniqube",
+      "kubectl apply -f ./postgres-service.yaml",
+      "kubectl apply -f ./postgres-deployment.yaml",
+      "kubectl apply -f ./postgres-cm1-configmap.yaml",
+      "kubectl apply -f ./postgres-claim0-persistentvolumeclaim.yaml",
+      "kubectl apply -f ./prometheus-service.yaml",
+      "kubectl apply -f ./prometheus-deployment.yaml",
+      "kubectl apply -f ./prometheus-cm0-configmap.yaml",
+      "kubectl apply -f ./node-exporter-service.yaml",
+      "kubectl apply -f ./node-exporter-deployment.yaml",
+      "kubectl apply -f ./node-exporter-claim0-persistentvolumeclaim.yaml",
+      "kubectl apply -f ./node-exporter-claim1-persistentvolumeclaim.yaml",
+      "kubectl apply -f ./pgadmin-service.yaml",
+      "kubectl apply -f ./pgadmin-deployment.yaml",
+      "kubectl apply -f ./pgadmin-data-persistentvolumeclaim.yaml",
+      "kubectl apply -f ./pgadmin-service.yaml",
+      "kubectl apply -f ./grafana-service.yaml",
+      "kubectl apply -f ./grafana-deployment.yaml",
+      "kubectl apply -f ./grafana-claim0-persistentvolumeclaim.yaml",
+      "kubectl apply -f ./backand-service.yaml",
+      "kubectl apply -f ./backand-deployment.yaml",
+      "kubectl apply -f ./backand-horizontalpodautoscaler.yaml",
+      "kubectl apply -f ./frontend-service.yaml",
+      "kubectl apply -f ./frontend-deployment.yaml",
+      "kubectl patch svc postgres -p '{\"spec\":{\"externalIPs\":[\"192.168.49.2\"]}}'",
+      "kubectl patch svc frontend -p '{\"spec\":{\"externalIPs\":[\"192.168.49.2\"]}}'",
+      "kubectl patch svc backand -p '{\"spec\":{\"externalIPs\":[\"192.168.49.2\"]}}'",
+      "kubectl patch svc pgadmin -p '{\"spec\":{\"externalIPs\":[\"192.168.49.2\"]}}'",
+      "kubectl patch svc grafana -p '{\"spec\":{\"externalIPs\":[\"192.168.49.2\"]}}'",
+      "kubectl patch svc prometheus -p '{\"spec\":{\"externalIPs\":[\"192.168.49.2\"]}}'",
+      "kubectl patch svc node-exporter -p '{\"spec\":{\"externalIPs\":[\"192.168.49.2\"]}}'",
+      "kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml"
+    ]    
+  }
+  depends_on = [null_resource.miniqube_run]
 }
 
 output "internal_ip_address_vm_1" {
