b9f2e074be268f256fbad5a602654221f50e1183
Денис Ермолаев <74070947+den251965@users.noreply.github.com>
2025-05-15 09:28:00 +0300

    Update server_rule.py

--- backend/server_rule.py
+++ backend/server_rule.py
@@ -14,7 +14,7 @@ pod_name = os.environ.get('POD_NAME')
 
 app = Flask(__name__)
 CORS(app)
-PrometheusMetrics(app, pod = '12')
+PrometheusMetrics(app, group_by = pod_name)
 
 # Метрика времени загрузки страницы
 frontend_page_load_time = Gauge('frontend_page_load_duration_seconds', 'Frontend page load duration')
