6bc5bb4bb7c04dcf3a81ba5f8b09a956622fedf8
Поцелуйко Иван Анатольевич <119080618+PeculiarRP@users.noreply.github.com>
2025-05-20 23:45:39 +0300

    k8s fix

--- frontend/src/utils/API.js
+++ frontend/src/utils/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     // baseURL: "http://127.0.0.1:8585",
-   baseURL: "http://" + window.location.hostname + ":8585",
+   baseURL: "http://backend:8585",
 })
 
 console.log("current ip : ",window.location.hostname+":8585");
