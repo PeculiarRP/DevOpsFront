a46dd7b7dad126a406eb41f2450d292582de515b
Поцелуйко Иван Анатольевич <119080618+PeculiarRP@users.noreply.github.com>
2025-05-27 21:26:05 +0300

    Update server_rule.py

--- backend/server_rule.py
+++ backend/server_rule.py
@@ -61,7 +61,7 @@ defaultcadri = Person("Иванов", "Иван", "Директор"), ("Пет�
 
 def ConnectorDB():
     # conn = psycopg2.connect('postgresql://postgres:cadri@localhost:5432/postgres')# For Windows
-    conn = psycopg2.connect('postgresql://postgres:cadri@192.168.49.2:5432/postgres')# For kuber
+    conn = psycopg2.connect('postgresql://postgres:cadri@postgres:5432/postgres')# For kuber
     # conn = psycopg2.connect('postgresql://postgres:cadri@postgres_container:5432/postgres')
     return conn
 
