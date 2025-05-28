9981a04746abe11f8723baa8512a23ed59c22db1
Поцелуйко Иван Анатольевич <119080618+PeculiarRP@users.noreply.github.com>
2025-05-29 01:00:00 +0300

    fix ci

--- frontend/src/utils/API.js
+++ frontend/src/utils/API.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
     // baseURL: "http://127.0.0.1:8585",
-   baseURL: "http://backend:8585",
+   baseURL: "http://localhost:8585",
 })
 
-console.log("current ip : http://backend:8585:8585");
+console.log("current ip : http://localhost:8585");
 
 export default api;
